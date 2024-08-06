df0864d31356b8575d61824634c6870daa2b8b42
tivjnb <butrovt06@gmail.com>
2024-08-06 23:37:47 +0300

    v1 подгрузка из бд

---
+++ status_service.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, jsonify
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, DateTime
+from datetime import datetime
+
+DB_NAME = 'pet_kafka_1'
+engine = create_engine(f"postgresql+psycopg2://user2:123@localhost:5432/{DB_NAME}")
+
+metadata = MetaData()
+task_table = Table(
+    'tasks', metadata,
+    Column('id', Integer(), primary_key=True),
+    Column('hash', String(200), nullable=False),
+    Column('name', String(100), nullable=False),
+    Column('duration', Integer(), nullable=False),
+    Column('status', String(50), default='Created'),
+    Column('created_on', DateTime(), default=datetime.now)
+)
+
+app = Flask(__name__)
+
+
+@app.route("/status/<hash>")
+def status_page(hash):
+    s = task_table.select().where(hash==task_table.c.hash)
+    with engine.connect() as conn:
+        r = conn.execute(s).fetchone()
+        print(r)
+        return jsonify(*r)
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000)
